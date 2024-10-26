5e2f6d298d3bf5d053ed8a60051819696709f3cf
gurdian0614 <ivan.gurdian@outlook.com>
2024-10-25 19:03:54 -0600

    Creando clase e implementacion

---
+++ PolimorfismoListasQ42024/Edificio.cs
@@ -0,0 +1,26 @@
+﻿
+namespace PolimorfismoListasQ42024
+{
+    public class Edificio
+    {
+        public string Tipo { get; set; }
+        public double Precio { get; set; }
+
+        public Edificio(string Tipo, double Precio) {
+            this.Tipo = Tipo;
+            this.Precio = Precio;
+        }
+
+        public void Vender()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Edificio de tipo {Tipo} ha sido vendido por el precio de L.{Precio.ToString("N2")}");
+        }
+
+        public void Vender(string Vendedor)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Edificio de tipo {Tipo} ha sido vendido por el precio de L.{Precio.ToString("N2")} y vendido por {Vendedor}");
+        }
+    }
+}
--- PolimorfismoListasQ42024/Program.cs
+++ PolimorfismoListasQ42024/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using PolimorfismoListasQ42024;
+
+string tipo, opcion;
+double precio;
+List<Edificio> edificioLista = new List<Edificio>();
+do
+{
+    Console.WriteLine("Ingrese el tipo de Edificio:");
+    tipo = Console.ReadLine();
+
+    Console.WriteLine("Ingrese el valor del Edificio:");
+    precio = Convert.ToDouble(Console.ReadLine());
+
+    Edificio edificio = new Edificio(tipo, precio);
+    edificioLista.Add(edificio);
+
+    Console.WriteLine("Desea continuar? (S/N o s/n)");
+    opcion = Console.ReadLine();
+
+} while (opcion.ToUpper() != "N");
+
+foreach (Edificio edificio in edificioLista)
+{
+    edificio.Vender();
+}
